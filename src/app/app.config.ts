6661132e5529c79aff6cdea3379337789eac0559
@dhanasekarraman <dhanasekar.raman@aravind.org>
2026-08-30 09:16:43 +0530

    Primeng default color changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,11 +8,29 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
-import Material from '@primeuix/themes/material';
+import { definePreset } from '@primeuix/themes';
 
 import { routes } from './app.routes';
 import { AuthService } from './services/security/auth.service';
 import { customHttpInterceptor } from './services/security/custom-http.interceptor';
+
+const AppPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '#eff7ff',
+      100: '#d9ecff',
+      200: '#b8d9f7',
+      300: '#8ebdf9',
+      400: '#5d9ed8',
+      500: '#2d6197',
+      600: '#0f4c81',
+      700: '#00355f',
+      800: '#002b4d',
+      900: '#001c37',
+      950: '#001224'
+    }
+  }
+});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -22,8 +40,9 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     providePrimeNG({
       theme: {
-        preset: Material,
+        preset: AppPreset,
         options: {
+          darkModeSelector: '.app-dark', // Disables automatic OS dark mode
           cssLayer: {
             name: 'primeng',
             order: 'theme, base, primeng, components, utilities',
