495c13d6dfc6517423d6630003308f08cd45ca41
@dhanasekarraman <dhanasekar.raman@aravind.org>
2026-09-02 19:34:26 +0530

    routes for Employees

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,9 +14,11 @@ import { MainLayoutComponent } from './layout/main-layout';
 import { DepartmentMasterComponent } from './features/departments/department-master';
 import { DepartmentFormComponent } from './features/departments/department-form';
 import { DepartmentViewComponent } from './features/departments/department-view';
-import { SpecializationMasterComponent } from './features/specialization/specialization-master';
+import { SpecializationMasterComponent as EmployeeMasterComponent } from './features/specialization/specialization-master';
 import { SpecializationFormComponent } from './features/specialization/specialization-form';
 import { SpecializationViewComponent } from './features/specialization/specialization-view';
+import { EmployeeFormComponent } from './features/employees/employee-form';
+import { EmployeeViewComponent } from './features/employees/employee-view';
 
 export const routes: Routes = [
   {
@@ -43,10 +45,14 @@ export const routes: Routes = [
       { path: 'departments/add', component: DepartmentFormComponent },
       { path: 'departments/:departmentId/edit', component: DepartmentFormComponent },
       { path: 'departments/:departmentId/view', component: DepartmentViewComponent },
-      { path: 'specializations', component: SpecializationMasterComponent },
+      { path: 'specializations', component: EmployeeMasterComponent },
       { path: 'specializations/add', component: SpecializationFormComponent },
       { path: 'specializations/:specializationId/edit', component: SpecializationFormComponent },
       { path: 'specializations/:specializationId/view', component: SpecializationViewComponent },
+      { path: 'employees', component: EmployeeMasterComponent },
+      { path: 'employees/add', component: EmployeeFormComponent },
+      { path: 'employees/:employeeId/edit', component: EmployeeFormComponent },
+      { path: 'employees/:employeeId/view', component: EmployeeViewComponent },
     ],
   },
   { path: '**', redirectTo: '/login' },
